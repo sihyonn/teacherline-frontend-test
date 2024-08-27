3dd554e2284043860284e1953dc3e1abda4417b8
sihyonn <sihyeon982@naver.com>
2024-08-27 15:45:35 +0900

    style: 피그마 시안에 맞춰 wrapper 스타일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,37 @@
 "use client";
-
-import ToDoList from "@/src/components/ToDoList";
 import styled from "@emotion/styled";
+import ToDoList from "@/src/components/ToDoList";
 
 export default function Home() {
   return (
     <S.Container>
-      <ToDoList />
+      <S.InnerWrap>
+        <ToDoList />
+      </S.InnerWrap>
     </S.Container>
   );
 }
 
 const S = {
   Container: styled.div`
-    //ToDo 나중에 레이아웃으로 전환
-    width: 35rem;
-    min-height: 74.8rem;
-    margin-top: 2rem;
-    margin-left: 2rem;
+    display: flex;
+    justify-content: center;
 
+    width: 100%;
+    height: 100vh;
+    padding: 2rem 4.8rem;
+    background-color: ${({ theme }) => theme.color.main};
+  `,
+
+  InnerWrap: styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    width: 35rem;
+    min-height: 74.8rem;
     padding: 2.5rem 1.5rem;
-    border: ${({ theme }) => theme.border};
+    border-radius: 1rem;
+    background-color: ${({ theme }) => theme.color.white};
   `,
 };
